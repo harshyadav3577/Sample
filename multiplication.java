b380a7d7a4b19adfa21724b43efe867244cd2abe
Harry <harshyadav3577@gmail.com>
2022-08-17 12:45:32 +0530

    added by Harsha

---
+++ multiplication.java
@@ -0,0 +1,16 @@
+class main
+{
+     public static void main(String args [])
+	 {
+	 
+	 int num1 =15;
+	 int num2 =3;
+	 
+	 int mul;
+	 
+	mul= num1*num2;
+	
+	System.out.println("Multiplication of two numbers is:"+mul);
+	 
+	 }
+}
